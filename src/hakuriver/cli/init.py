cfe0b49818af40cef9e230b14cbd2c2b71ad90fd
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2025-04-17 21:31:05 +0800

    Update init.py

--- src/hakuriver/cli/init.py
+++ src/hakuriver/cli/init.py
@@ -8,7 +8,7 @@ def main():
     if not os.path.exists(default_config_path):
         os.makedirs(default_config_path)
 
-    base_dir = os.path.dirname(__file__).split("/src")[0]
+    base_dir = os.path.dirname(__file__).split("src")[0]
     config_path = os.path.join(base_dir, "src", "utils", "default_config.toml")
     default_config_file = os.path.join(default_config_path, "config.toml")
     if not os.path.exists(default_config_file):
